35b188db9039015d62e7921173f9d7446fd5eded
flaviomahoney <flavio@mahoneyt.com>
2021-06-30 09:20:49 -0300

    Added active storage and I'm creating 'new post'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,31 @@
 class PostsController < ApplicationController
-
     def index
         @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        authorize @post
+        if @post.save
+          flash[:success] = "Object successfully created"
+          redirect_to @post
+        else
+          flash[:alert] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    private
+
+    def set_post
+        @post = Post.find(params[:id])
+      end
+    
+    def post_params
+        params.require(:post).permit(:description, :photo)
     end
 end
